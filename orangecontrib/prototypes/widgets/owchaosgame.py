97b3132dba3b74b9dc934317047535eb38a54b84
Jernej Kozelj <kozki8@gmail.com>
2017-01-04 10:44:56 +0100

    Added enums for scoring and kmer_lengths.

--- orangecontrib/prototypes/widgets/owchaosgame.py
+++ orangecontrib/prototypes/widgets/owchaosgame.py
@@ -5,6 +5,13 @@ from Orange.data import Table
 from Orange.widgets import widget, gui, settings
 from ..chaos import chaosgame
 
+import collections
+from enum import Enum
+from PyQt4.QtCore import Qt
+from Orange.data import Table
+from Orange.widgets import widget, gui, settings
+
+"""
 KMER_LENGTHS = (
         ('2', 2),
         ('3', 3),
@@ -18,53 +25,57 @@ SCORINGS = (
     ('probability', 1),
     ('log odds', 2)
 )
+"""
+class KmerLengths:
+    two, three, four, five, six = range(5)
 
-if __name__ == '__main__':
-    class OWChaosGame(widget.OWWidget):
-        name = "Chaos Game"
-        description = ""
-        icon = ""
-        priority = 9999
+class Scorings:
+    raw_count, probability, log_odds = range(3)
 
-        inputs = [("Sequence", Table, "set_data")]
-        outputs = []
+class OWChaosGame(widget.OWWidget):
+    name = "Chaos Game"
+    description = ""
+    icon = ""
+    priority = 9999
 
-        #kmer_length = settings.Setting(KMER_LENGTHS[0][1])
-        #scoring = settings.Setting(SCORINGS[0][1])
-        kmer_length = 2
-        scoring = 0
-        chaos = np.empty
+    inputs = [("Sequence", Table, "set_data")]
+    outputs = []
 
-        def __init__(self):
-            super().__init__()
+    #kmer_length = settings.Setting(KmerLengths.two)
+    #scoring = settings.Setting(Scorings.raw_count)
 
-            self.controlBox = gui.widgetBox(self.controlArea, 'Controls')
-            self.sequence = None
+    kmer_length = KmerLengths.two
+    scoring = Scorings.raw_counts
 
-            def _on_kmer_length_changed():
-                pass
+    def __init__(self):
+        super().__init__()
 
-            gui.comboBox(self.controlBox, self, 'kmer_length',
-                         orientation=Qt.Horizontal,
-                         label='Kmer length:',
-                         items=[i[0] for i in KMER_LENGTHS],
-                         callback=_on_kmer_length_changed)
+        self.controlBox = gui.widgetBox(self.controlArea, 'Controls')
+        self.sequence = None
 
-            def _on_scoring_changed():
-                pass
+    def _on_kmer_length_changed():
+            pass
 
-            gui.comboBox(self.controlBox, self, 'scoring',
-                         orientation=Qt.Horizontal,
-                         label='Scoring:',
-                         items=[i[0] for i in SCORINGS],
-                         callback=_on_scoring_changed())
+    gui.comboBox(self.controlBox, self, 'kmer_length',
+                 orientation=Qt.Horizontal,
+                 label='Kmer length:',
+                 items=[i[0] for i in KmerLengths],
+                 callback=_on_kmer_length_changed)
 
-        def set_data(self, data):
-            self.sequence = ''.join([d.list[0] for d in data])
-            self.cgr()
+    def _on_scoring_changed():
+        pass
 
-        def cgr(self):
-            #TODO: switch for probabilities, log-odds..
-            probabilities = chaosgame.raw_count(self.sequence, self.kmer_length)
+    gui.comboBox(self.controlBox, self, 'scoring',
+                 orientation=Qt.Horizontal,
+                 label='Scoring:',
+                 items=[i[0] for i in Scorings],
+                 callback=_on_scoring_changed())
 
-            self.chaos = chaosgame.cgr(probabilities, self.kmer_length)
+    def set_data(self, data):
+        self.sequence = ''.join([d.list[0] for d in data])
+        self.cgr()
+
+    def cgr(self):
+        #TODO: switch for probabilities, log-odds..
+        probabilities = chaosgame.raw_count(self.sequence, self.kmer_length)
+        self.chaos = chaosgame.cgr(probabilities, self.kmer_length)
