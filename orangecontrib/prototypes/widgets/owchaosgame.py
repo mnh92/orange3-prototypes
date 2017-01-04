5c6b38509a3c2b229e24bd1c6a200eb3abe1c90f
Marko Novak Hindel <marko.n.hindel@gmail.com>
2017-01-04 13:46:49 +0100

    added cgr plot display

--- orangecontrib/prototypes/widgets/owchaosgame.py
+++ orangecontrib/prototypes/widgets/owchaosgame.py
@@ -1,36 +1,20 @@
 import numpy as np
+import pyqtgraph as pg
 
-from PyQt4.QtCore import Qt
-from Orange.data import Table
-from Orange.widgets import widget, gui, settings
 from ..chaos import chaosgame
 
-import collections
-from enum import Enum
 from PyQt4.QtCore import Qt
 from Orange.data import Table
 from Orange.widgets import widget, gui, settings
 
-"""
-KMER_LENGTHS = (
-        ('2', 2),
-        ('3', 3),
-        ('4', 4),
-        ('5', 5),
-        ('6', 6)
-    )
 
-SCORINGS = (
-    ('raw count', 0),
-    ('probability', 1),
-    ('log odds', 2)
-)
-"""
 class KmerLengths:
     two, three, four, five, six = range(5)
 
+
 class Scorings:
     raw_count, probability, log_odds = range(3)
+
 
 class OWChaosGame(widget.OWWidget):
     name = "Chaos Game"
@@ -41,41 +25,64 @@ class OWChaosGame(widget.OWWidget):
     inputs = [("Sequence", Table, "set_data")]
     outputs = []
 
-    #kmer_length = settings.Setting(KmerLengths.two)
-    #scoring = settings.Setting(Scorings.raw_count)
+    #kmer_length = settings.Setting(2)
+    scoring = settings.Setting(0)
 
-    kmer_length = KmerLengths.two
-    scoring = Scorings.raw_counts
+    kmer_length = 2
 
     def __init__(self):
         super().__init__()
 
         self.controlBox = gui.widgetBox(self.controlArea, 'Controls')
         self.sequence = None
+        self.chaos = np.zeros
 
-    def _on_kmer_length_changed():
+        def _on_kmer_length_changed():
+                pass
+
+        """
+        gui.comboBox(self.controlBox, self, 'kmer_length',
+                     orientation=Qt.Horizontal,
+                     label='Kmer length:',
+                     items=[i[0] for i in KmerLengths],
+                     callback=_on_kmer_length_changed)
+        """
+
+        def _on_scoring_changed():
             pass
 
-    gui.comboBox(self.controlBox, self, 'kmer_length',
-                 orientation=Qt.Horizontal,
-                 label='Kmer length:',
-                 items=[i[0] for i in KmerLengths],
-                 callback=_on_kmer_length_changed)
+        """
+        gui.comboBox(self.controlBox, self, 'scoring',
+                     orientation=Qt.Horizontal,
+                     label='Scoring:',
+                     items=[i[0] for i in Scorings],
+                     callback=_on_scoring_changed())
+        """
 
-    def _on_scoring_changed():
-        pass
+        self.imview = pg.ImageView()
 
-    gui.comboBox(self.controlBox, self, 'scoring',
-                 orientation=Qt.Horizontal,
-                 label='Scoring:',
-                 items=[i[0] for i in Scorings],
-                 callback=_on_scoring_changed())
+        colors = [
+            (255, 255, 255),
+            (0, 0, 0)
+        ]
+
+        colormap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 2), color=colors)
+        self.imview.setColorMap(colormap)
+
+        self.mainArea.layout().addWidget(self.imview)
 
     def set_data(self, data):
+        self.imview.clear()
         self.sequence = ''.join([d.list[0] for d in data])
-        self.cgr()
 
-    def cgr(self):
+        self.plot_cgr()
+
+    def __cgr(self):
         #TODO: switch for probabilities, log-odds..
         probabilities = chaosgame.raw_count(self.sequence, self.kmer_length)
-        self.chaos = chaosgame.cgr(probabilities, self.kmer_length)
+        chaos = chaosgame.cgr(probabilities, self.kmer_length)
+        return chaos
+
+    def plot_cgr(self):
+        chaos = self.__cgr()
+        self.imview.setImage(chaos)
