2642acbc7b42c70193f68a3a87c98c57c9264d8a
Marko Novak Hindel <marko.n.hindel@gmail.com>
2017-01-04 10:02:15 +0100

    Moved cgr functions to separate source file

---
+++ orangecontrib/prototypes/chaos/chaosgame.py
@@ -0,0 +1,35 @@
+import collections
+import math
+import numpy as np
+
+
+def raw_count(sequence, kmer_length):
+    count = collections.defaultdict(int)
+    for i in range(len(sequence) - (kmer_length - 1)):
+        count[sequence[i:i + kmer_length]] += 1
+    return count
+
+
+def cgr(probabilities, kmer_length):
+    size = int(math.sqrt(4 ** kmer_length))
+    chaos = np.zeros((size, size))
+
+    for kmer, prob in probabilities.items():
+        x_max = size
+        y_max = size
+        x_pos = 1
+        y_pos = 1
+
+        for c in kmer:
+            if c == 'T':
+                x_pos += x_max / 2
+            elif c == 'C':
+                y_pos += y_max / 2
+            elif c == 'G':
+                x_pos += x_max / 2
+                y_pos += y_max / 2
+
+            x_max /= 2
+            y_max /= 2
+        chaos[int(x_pos) - 1, int(y_pos) - 1] = prob
+    return chaos
--- orangecontrib/prototypes/widgets/owchaosgame.py
+++ orangecontrib/prototypes/widgets/owchaosgame.py
@@ -1,12 +1,9 @@
-import collections
-import pylab as pl
-import math
 import numpy as np
 
 from PyQt4.QtCore import Qt
 from Orange.data import Table
 from Orange.widgets import widget, gui, settings
-
+from ..chaos import chaosgame
 
 KMER_LENGTHS = (
         ('2', 2),
@@ -22,81 +19,52 @@ SCORINGS = (
     ('log odds', 2)
 )
 
+if __name__ == '__main__':
+    class OWChaosGame(widget.OWWidget):
+        name = "Chaos Game"
+        description = ""
+        icon = ""
+        priority = 9999
 
-class OWChaosGame(widget.OWWidget):
-    name = "Chaos Game"
-    description = ""
-    icon = ""
-    priority = 9999
+        inputs = [("Sequence", Table, "set_data")]
+        outputs = []
 
-    inputs = [("Sequence", Table, "set_data")]
-    outputs = []
+        #kmer_length = settings.Setting(KMER_LENGTHS[0][1])
+        #scoring = settings.Setting(SCORINGS[0][1])
+        kmer_length = 2
+        scoring = 0
+        chaos = np.empty
 
-    #kmer_length = settings.Setting(KMER_LENGTHS[0][1])
-    #scoring = settings.Setting(SCORINGS[0][1])
-    kmer_length = 2
-    scoring = 0
-    chaos = np.empty
+        def __init__(self):
+            super().__init__()
 
-    def __init__(self):
-        super().__init__()
+            self.controlBox = gui.widgetBox(self.controlArea, 'Controls')
+            self.sequence = None
 
-        self.controlBox = gui.widgetBox(self.controlArea, 'Controls')
-        self.sequence = None
+            def _on_kmer_length_changed():
+                pass
 
-        def _on_kmer_length_changed():
-            pass
+            gui.comboBox(self.controlBox, self, 'kmer_length',
+                         orientation=Qt.Horizontal,
+                         label='Kmer length:',
+                         items=[i[0] for i in KMER_LENGTHS],
+                         callback=_on_kmer_length_changed)
 
-        gui.comboBox(self.controlBox, self, 'kmer_length',
-                     orientation=Qt.Horizontal,
-                     label='Kmer length:',
-                     items=[i[0] for i in KMER_LENGTHS],
-                     callback=_on_kmer_length_changed)
+            def _on_scoring_changed():
+                pass
 
-        def _on_scoring_changed():
-            pass
+            gui.comboBox(self.controlBox, self, 'scoring',
+                         orientation=Qt.Horizontal,
+                         label='Scoring:',
+                         items=[i[0] for i in SCORINGS],
+                         callback=_on_scoring_changed())
 
-        gui.comboBox(self.controlBox, self, 'scoring',
-                     orientation=Qt.Horizontal,
-                     label='Scoring:',
-                     items=[i[0] for i in SCORINGS],
-                     callback=_on_scoring_changed())
+        def set_data(self, data):
+            self.sequence = ''.join([d.list[0] for d in data])
+            self.cgr()
 
-        self.infoLabel = gui.widgetLabel(self.controlBox, label='No input')
+        def cgr(self):
+            #TODO: switch for probabilities, log-odds..
+            probabilities = chaosgame.raw_count(self.sequence, self.kmer_length)
 
-    def set_data(self, data):
-        self.sequence = ''.join([d.list[0] for d in data])
-        self.cgr()
-
-    def __raw_count(self):
-        count = collections.defaultdict(int)
-        for i in range(len(self.sequence) - (self.kmer_length - 1)):
-            count[self.sequence[i:i + self.kmer_length]] += 1
-        return count
-
-    def cgr(self):
-        size = int(math.sqrt(4**self.kmer_length))
-        chaos = np.zeros((size, size))
-
-        #TODO: switch for probabilities, log-odds...
-        probs = self.__raw_count()
-
-        for kmer, prob in probs.items():
-            x_max = size
-            y_max = size
-            x_pos = 1
-            y_pos = 1
-
-            for c in kmer:
-                if c == 'T':
-                    x_pos += x_max / 2
-                elif c == 'C':
-                    y_pos += y_max / 2
-                elif c == 'G':
-                    x_pos += x_max / 2
-                    y_pos += y_max / 2
-
-                x_max /= 2
-                y_max /= 2
-            chaos[int(x_pos) - 1, int(y_pos) - 1] = prob
-        self.chaos = chaos
+            self.chaos = chaosgame.cgr(probabilities, self.kmer_length)
